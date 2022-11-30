357bef72a02671b125a74b36d5755b43cd5c16cd
KanHar <knaan.harpaz@gmail.com>
2022-11-30 09:26:31 +0200

    WIP

---
+++ naive_fft/iv_performance_analysis/plot_performance.py
@@ -0,0 +1,39 @@
+import random
+import time
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from naive_fft.ii_poly_multiplication.evaluate_poly import evaluate_poly
+from naive_fft.ii_poly_multiplication.values_to_poly import values_to_poly
+
+MIN_TIME_FOR_ANALYSIS = 0.01  # 0.01 second per tested size
+
+
+sns.set_theme()
+
+def get_sample_performance(start_size: int, end_size: int, step_size: int) -> None:
+    samples: list[tuple[int, float]] = []
+    size = start_size
+    while size < end_size:
+        random_poly: list[complex] = []
+        for _ in range(size):
+            random_poly.append(random.random() * 2 - 1 + 1j * (random.random() * 2 - 1))
+        t_0 = time.time()
+        t_1 = time.time()
+        i = 0
+        while t_1 - t_0 < MIN_TIME_FOR_ANALYSIS:
+            values_to_poly(evaluate_poly(random_poly))
+            i += 1
+            t_1 = time.time()
+        average_time = (t_1 - t_0) / i
+        samples.append((size, average_time))
+        size += step_size
+    return samples
+
+
+def plot_1_to_100():
+    samples = get_sample_performance(1, 201, 1)
+    x,y = zip(*samples)
+    plt.plot(x,y)
+    plt.show()
