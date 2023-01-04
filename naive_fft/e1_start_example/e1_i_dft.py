dd505fa4fc86e0bb2d841505fce0cbc874b01319
KanHar <knaan.harpaz@gmail.com>
2023-01-04 16:33:55 +0200

    WIP

--- naive_fft/e1_start_example/e1_i_dft.py
+++ naive_fft/e1_start_example/e1_i_dft.py
@@ -29,14 +29,32 @@ def dft_exp(array: List[complex]) -> List[complex]:
     return result
 
 
+def dft_poly(array: List[complex]) -> List[complex]:
+    result: List[complex] = []
+    total_length = len(array)
+    root_of_unity = cmath.exp(-(0 + 1j) * 2 * math.pi / total_length)
+    # We use the conjugate instead of root of unity to use the same convention as numpy
+    # Use the following to get the root of unity instead:
+    # root_of_unity = cmath.exp((0 + 1j) * 2 * math.pi / total_length)
+    for i in range(total_length):
+        accumulator = 0 + 0j
+        root_of_unity_power = root_of_unity ** i
+        for n in range(total_length):
+            accumulator += array[n] * root_of_unity_power ** n
+        result.append(accumulator)
+    return result
+
+
 def main() -> None:
     sample_input = [1 + 0j, 1 + 0j, 0 + 0j, 0 + 0j]
     trig_dft_result = dft_trig(sample_input)
     exp_dft_result = dft_exp(sample_input)
     numpy_fft_result = list(numpy_fft(sample_input))
+    poly_dft_result = dft_poly(sample_input)
     print(f"Trig DFT: {trig_dft_result}")
     print(f"Exp DFT: {exp_dft_result}")
     print(f"Numpy FFT: {numpy_fft_result}")
+    print(f"Poly DFT: {poly_dft_result}")
 
 
 if __name__ == "__main__":
