61ef4c3d61b306b4fa3064f61e3a4248d2aa5b6f
KanHar <knaan.harpaz@gmail.com>
2023-01-04 17:22:01 +0200

    Add phase recovery

--- naive_fft/e1_start_example/e1_ii_dirty_wave.py
+++ naive_fft/e1_start_example/e1_ii_dirty_wave.py
@@ -9,13 +9,14 @@ from naive_fft.iii_fft.fft import fft
 BASE_FREQUENCY = 100 * 2 * math.pi
 NUM_SAMPLES = 1000
 NOISE_MAGNITUDE = 10
+PHASE = 0.5
 
 
 def main() -> None:
     samples: List[complex] = []
     for i in range(NUM_SAMPLES):
         samples.append(
-            math.sin(BASE_FREQUENCY * i / NUM_SAMPLES)
+            math.sin(BASE_FREQUENCY * i / NUM_SAMPLES + PHASE)
             + random.random() * NOISE_MAGNITUDE
             - NOISE_MAGNITUDE / 2
         )
@@ -24,6 +25,8 @@ def main() -> None:
     samples_fft = fft(samples)
     plt.plot([abs(x) for x in samples_fft])
     plt.show()  # Can see frequency very clearly. Phase is lost due to absolute value, but can be recovered
+    phase = math.pi / 2 + math.atan(samples_fft[100].imag / samples_fft[100].real)
+    print(f"Phase: {phase}")
 
 
 if __name__ == "__main__":
